939c7199eb3b4118ee47762a2384b5835b567aa5
OUSSAMA BENCHKRON SAID <obenchkr@student.1337.ma>
2024-10-08 10:18:01 +0100

    use assert for tests

--- ex00/easyfind.h
+++ ex00/easyfind.h
@@ -18,10 +18,8 @@ typename T::const_iterator easyfind(const T &c, int v) {
     typename T::const_iterator it;
     for (it = c.begin(); it != c.end(); it++) {
         if (*it == v) {
-            std::cout << "value " << *it << " found!" << std::endl;
             return it;
         }
     }
-    std::cout << "value " << *it << " not found!" << std::endl;
     return c.end();
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,39 +3,52 @@
 #include <vector>
 #include <list>
 #include <deque>
+#include <cassert>
+
+void test_vector() {
+    int values[5] = {10, 1337, 42, 0, -10};
+    std::vector<int> nbrs_vec(values, values + 5);
+    assert(easyfind(nbrs_vec, 10) != nbrs_vec.end());
+    assert(easyfind(nbrs_vec, 1337) != nbrs_vec.end());
+    assert(easyfind(nbrs_vec, -10) != nbrs_vec.end());
+    assert(easyfind(nbrs_vec, 11) == nbrs_vec.end());
+    std::cout << "=> Testing with vector passed!\n";
+}
+
+void test_const_vector() {
+    int values[5] = {10, 1337, 42, 0, -10};
+    const std::vector<int> nbrs_vec(values, values + 5);
+    assert(easyfind(nbrs_vec, 10) != nbrs_vec.end());
+    assert(easyfind(nbrs_vec, 1337) != nbrs_vec.end());
+    assert(easyfind(nbrs_vec, -10) != nbrs_vec.end());
+    assert(easyfind(nbrs_vec, 11) == nbrs_vec.end());
+    std::cout << "=> Testing with const vector passed!\n";
+}
+
+void test_list() {
+    int values[5] = {10, 1337, 42, 0, -10};
+    const std::list<int> nbrs_list(values, values + 5);
+    assert(easyfind(nbrs_list, 10) != nbrs_list.end());
+    assert(easyfind(nbrs_list, 1337) != nbrs_list.end());
+    assert(easyfind(nbrs_list, -10) != nbrs_list.end());
+    assert(easyfind(nbrs_list, 11) == nbrs_list.end());
+    std::cout << "=> Testing with list passed!\n";
+}
+
+void test_deque() {
+    int values[5] = {10, 1337, 42, 0, -10};
+    const std::list<int> nbrs_list(values, values + 5);
+    assert(easyfind(nbrs_list, 10) != nbrs_list.end());
+    assert(easyfind(nbrs_list, 1337) != nbrs_list.end());
+    assert(easyfind(nbrs_list, -10) != nbrs_list.end());
+    assert(easyfind(nbrs_list, 11) == nbrs_list.end());
+    std::cout << "=> Testing with double ended queue passed!\n";
+}
 
 int main(void) {
-    int values[5] = {10, 1337, 42, 0, -10};
-
-    std::cout << "=> Testing with vector\n";
-    std::vector<int> nbrs_vec(values, values + 5);
-
-    easyfind(nbrs_vec, 10);
-    easyfind(nbrs_vec, 1337);
-    easyfind(nbrs_vec, -10);
-    easyfind(nbrs_vec, 11);
-
-    std::cout << "=> Testing with const vector\n";
-    const std::vector<int> const_nbrs_vec(values, values + 5);
-
-    easyfind(const_nbrs_vec, 10);
-    easyfind(const_nbrs_vec, 1337);
-    easyfind(const_nbrs_vec, -10);
-    easyfind(const_nbrs_vec, 11);
-
-    std::cout << "=> Testing with list\n";
-    std::list<int> nbrs_lst(values, values + 5);
-    easyfind(const_nbrs_vec, 10);
-    easyfind(const_nbrs_vec, 1337);
-    easyfind(const_nbrs_vec, -10);
-    easyfind(const_nbrs_vec, 11);
-
-    std::cout << "=> testing with double ended queue\n";
-    std::deque<int> nbrs_deque(values, values + 5);
-    easyfind(const_nbrs_vec, 10);
-    easyfind(const_nbrs_vec, 1337);
-    easyfind(const_nbrs_vec, -10);
-    easyfind(const_nbrs_vec, 11);
-
+    test_vector();
+    test_const_vector();
+    test_list();
+    test_deque();
     return 0;
 }
