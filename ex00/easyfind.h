54e773be3c72ea74c10c64e2e8e85cd664716646
OUSSAMA BENCHKRON SAID <obenchkr@student.1337.ma>
2024-10-08 19:39:18 +0100

    Refactor easyfind.h to use std::find algorithm for searching elements in a container

--- ex00/easyfind.h
+++ ex00/easyfind.h
@@ -2,24 +2,16 @@
 
 #include <iterator>
 #include <iostream>
+#include <algorithm>
 
 template<typename Tp>
 typename Tp::iterator easyfind(Tp &c, int v) {
     typename Tp::iterator it;
-    for (it = c.begin(); it != c.end(); it++) {
-        if (*it == v)
-            return it;
-    }
-    return c.end();
+    return std::find(c.begin(), c.end(), v);
 }
 
 template<typename Tp>
 typename Tp::const_iterator easyfind(const Tp &c, int v) {
     typename Tp::const_iterator it;
-    for (it = c.begin(); it != c.end(); it++) {
-        if (*it == v) {
-            return it;
-        }
-    }
-    return c.end();
+    return std::find(c.begin(), c.end(), v);
 }
